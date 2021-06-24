3f5ea8dd86f1a1bae7a011dbab4dcc2ba7df52c6
Abel Limas <limas9@icloud.com>
2021-06-24 15:41:12 -0500

    created customer model inherting from user auth class

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Customer Class0
+class Customer(User):
+# class Customer(models.Model):
+    # first_name = models.CharField(max_length=50, null=False)
+    # last_name = models.CharField(max_length=50, null=False)
+    # email = models.EmailField()
+    # password = models.CharField(max_length=10, null=False)
+    question_1 = models.CharField(max_length=50, null=False)
+    answer_1 = models.CharField(max_length=50, null=False)
+    question_2 = models.CharField(max_length=50, null=False)
+    answer_2 = models.CharField(max_length=50, null=False)
+    is_provider = models.BooleanField(null=False)
