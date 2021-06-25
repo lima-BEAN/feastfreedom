d0996c4dc3c7dc5f007fc43ea10d620017622281
aqil56 <profit1232003@gmail.com>
2021-06-25 11:28:50 -0500

    kitchen1

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from dynamorm import DynaModel
+from marshmallow import fields
+from django.conf import settings
 
-# Create your models here.
+class Kitchen(DynaModel):
+    class Table:
+        resource_kwargs = {
+            'endpoint_url' : settings.DB_ENDPOINT
+        }
+        name = settings.DB_TABLE
+        hash_key = 'model_name'
+        read = 25
+        write = 5
+    
+    class Schema:
+        model_name = fields.String()
+        name = fields.String()
+        data = fields.String()
+        id = fields.Integer()
